193ed3fb4529c79c2b682fa647a256c86b68be0e
Daniel.J <edani.5420@gmail.com>
2026-06-05 17:00:01 +0530

    Update dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -14,7 +14,7 @@ st.set_page_config(
 # -----------------------------
 # LOAD DATA
 # -----------------------------
-df = pd.read_csv("../data/employees_clean.csv")
+df = pd.read_csv("data/employees_clean.csv")
 
 # -----------------------------
 # COLORS
